4384aaa95767ab5562aaaafa8e7e50875efcd392
Federico Sangati <federico.sangati@oist.jp>
2024-09-19 00:31:01 +0900

    refactoring conv2d fft

--- gol/pure/ca_conv.py
+++ gol/pure/ca_conv.py
@@ -25,7 +25,11 @@ class Automata:
     rule[1]: '0->1' based on "1" neighbours (can't contain 0)
     torch_device: None or in ["cpu", "cuda", "mps"]
     '''
-    def __init__(self, shape, board, neighborhood, rule, torus=True, torch_device = None):
+    def __init__(
+            self, board, neighborhood, rule, torus=True,
+            use_fft = False, # otherwise conv2d
+            torch_device = None,
+    ):
 
         assert (
             0 not in rule[0]
@@ -37,10 +41,11 @@ class Automata:
             torch_device in [None, "cpu", "cuda", "mps"]
         ), f"torch_device ({torch_device}) not recognized"
 
-        self.torch_device = torch_device
-        self.use_torch = torch_device is not None
-
+        # board - the grid
         self.board = board
+
+        # neighborhood (e.g, 3x3 in GoL)
+        self.neighborhood = neighborhood
 
         self.shape_x, _, = self.height, self.width = self.shape = self.board.shape
 
@@ -48,45 +53,89 @@ class Automata:
 
         self.minus_shape_x_half_plus_one =  - int(self.shape_x / 2) + 1
 
+        self.rule = rule
         self.torus = torus
 
-        self.neighborhood = neighborhood
-        nh, nw = self.neighborhood.shape # say (3,3) for Conway's GoL
+        # torch
+        self.torch_device = torch_device
+        self.use_torch = torch_device is not None
 
-        # create the kernal (init as zero)
-        # put neighborhood mask in the middle (everything else is 0.)
-        # TODO check:
-        # - not always perfectly centered
-        # - e.g., with 3x3 neighborhood and grid size being even (power of two)
-        self.kernal = np.zeros(shape) # kernal has same shape of board, say (256,256)
-        self.kernal[
-            (shape[0] - nh - 1) // 2 : (shape[0] + nh) // 2,
-            (shape[1] - nw - 1) // 2 : (shape[1] + nw) // 2
-        ] = self.neighborhood
+        if use_fft:
+            nh, nw = self.neighborhood.shape # say (3,3) for Conway's GoL
 
-        self.rule = rule
+            # create the FFT kernal (init as zero)
+            # put neighborhood mask in the middle (everything else is 0.)
+            # TODO check:
+            # - not always perfectly centered
+            # - e.g., with 3x3 neighborhood and grid size being even (power of two)
+            self.kernal = np.zeros(self.shape) # kernal has same shape of board, say (256,256)
+            self.kernal[
+                (self.shape[0] - nh - 1) // 2 : (self.shape[0] + nh) // 2,
+                (self.shape[1] - nw - 1) // 2 : (self.shape[1] + nw) // 2
+            ] = self.neighborhood
 
-        # convert arrays to tensor if using torch
         if self.use_torch:
+            # need to convert arrays to tensor when using torch
             # print('cuda available:', torch.cuda.is_available())
             # print('mps available:', torch.backends.mps.is_available())
             torch_device = torch.device(torch_device)
             torch.set_default_device(torch_device)
-            self.rule = [torch.FloatTensor(r).to(torch_device) for r in self.rule] # need to convert each rule to float tensor
-            self.neighborhood = torch.from_numpy(self.neighborhood).float().to(torch_device) # make sure it's float
-            self.board = torch.from_numpy(self.board).float().to(torch_device) # make sure it's float
-            self.kernal = torch.from_numpy(self.kernal).float().to(torch_device)
-            self.kernal_ft = torch_fft2(self.kernal)
+
+            if use_fft:
+                # less efficient (buth worth mentioning)
+                self.torch_conv_func = self.torch_conv_fft
+                self.board = torch.from_numpy(self.board).to(torch_device)
+                self.rule = [torch.IntTensor(r).to(torch_device) for r in self.rule] # int
+                self.kernal = torch.from_numpy(self.kernal).to(torch_device)
+                self.kernal_ft = torch_fft2(self.kernal)
+                self.rule_dtype = torch.int32
+            else:
+                # use conv2d (more efficient) - TODO: check why only works on floats
+                # need to convert each rule to float tensor
+                self.torch_conv_func = self.torch_conv_conv2d
+                self.board = torch.from_numpy(self.board).float().to(torch_device)
+                self.rule = [torch.FloatTensor(r).to(torch_device) for r in self.rule] # float
+                self.neighborhood = torch.from_numpy(self.neighborhood).float().to(torch_device)
+                self.rule_dtype = torch.float32
         else:
-            self.kernal_ft = np_fft2(self.kernal) # same shape but floating numbers
-
-
-
+            # numpy
+            if use_fft:
+                # less efficient (buth worth mentioning)
+                self.numpy_conv_func = self.np_conv_fft
+                self.kernal_ft = np_fft2(self.kernal) # same shape but floating numbers
+            else:
+                # use conv2d (more efficient)
+                self.numpy_conv_func = self.np_conv_conv2d
 
     '''
     Main count_real operation in a single time-step
+    Based on numpy and conv2d
     '''
-    def fft_convolve2d(self):
+    def np_conv_conv2d(self):
+
+        # create two more dims
+        board_conv = self.board[None,None,:,:]
+        nb_conv = self.neighborhood[None,None,:,:]
+
+        # the conv2d step
+        # TODO: find numpy function, the following doesn't work
+        counts_int = np.nn.functional.conv2d(board_conv, nb_conv, padding='same')
+
+        # taking only first two channels in first two dim
+        counts_int = counts_int[0,0,:,:]
+
+        # rolling over the boundaries
+        # see https://en.wikipedia.org/wiki/Torus
+        if self.torus:
+            counts_int = self.np_roll_boundaries(counts_int)
+
+        return counts_int
+
+    '''
+    Main count_real operation in a single time-step
+    Based on numpy and fft
+    '''
+    def np_conv_fft(self):
         # fft2 same shape but floating numbers
         board_ft = np_fft2(self.board)
 
@@ -99,72 +148,117 @@ class Automata:
         # round real part to closest integer
         counts_int = np.rint(count_real)
 
-        # double check
-        # counts_round = count_real.round()
-        # assert np.array_equal(counts_int, counts_round)
-        # return counts_round
-
         # rolling over the boundaries
         # see https://en.wikipedia.org/wiki/Torus
         if self.torus:
-            counts_int = np.roll(
-                counts_int,
-                self.minus_shape_x_half_plus_one,
-                axis=(0,1)
-            )
+            counts_int = self.np_roll_boundaries(counts_int)
 
         return counts_int
 
     '''
-    Main count_real operation in a single time-step (torch version)
+    Rolling over the boundaries (numpy version)
+    see https://en.wikipedia.org/wiki/Torus
     '''
-    def fft_convolve2d_torch(self):
+    def np_roll_boundaries(self, counts_int):
+        return np.roll(
+            counts_int,
+            self.minus_shape_x_half_plus_one,
+            axis=(0,1)
+        )
+
+    '''
+    Main count_real operation in a single time-step (torch version)
+    Usign FFT (not as efficient as torch.nn.functional.conv2d)
+    '''
+    def torch_conv_fft(self):
+        # fft2 same shape but floating numbers
+        board_ft = torch_fft2(self.board)
+
+        # inverted fft2 (complex numbers)
+        inverted = torch_ifft2(board_ft * self.kernal_ft)
+
+        # get the real part of the complex argument (real number)
+        count_real = torch.real(inverted)
+
+        # round real part to closest integer
+        counts_int = torch.round(count_real).int()
+
+        if self.torus:
+            # rolling over the boundaries
+            counts_int = self.torch_roll_boundaries(counts_int)
+
+        return counts_int
+
+    '''
+    Main count_real operation in a single time-step
+    torch version using conv2d (more efficient)
+    '''
+    def torch_conv_conv2d(self):
 
         # create two more dims
         board_conv = self.board[None,None,:,:]
         nb_conv = self.neighborhood[None,None,:,:]
 
+        # the conv2d step
         counts_int = torch.nn.functional.conv2d(board_conv, nb_conv, padding='same')
-        counts_int = counts_int[0,0,:,:] # taking only first two channels in first two dim
 
-        # fft2 same shape but floating numbers
-        # board_ft = torch_fft2(self.board)
+        # taking only first two channels in first two dim
+        counts_int = counts_int[0,0,:,:]
 
-        # inverted fft2 (complex numbers)
-        # inverted = torch_ifft2(board_ft * self.kernal_ft)
-
-        # get the real part of the complex argument (real number)
-        # count_real = torch.real(inverted)
-
-        # round real part to closest integer
-        # counts_int = torch.round(count_real).int()
-
-        # double check
-        # counts_round = count_real.round()
-        # assert np.array_equal(counts_int, counts_round)
-        # return counts_round
-
-        # rolling over the boundaries
-        # see https://en.wikipedia.org/wiki/Torus
         if self.torus:
-            counts_int = torch.roll(
-                counts_int,
-                (
-                    self.minus_shape_x_half_plus_one,
-                    self.minus_shape_x_half_plus_one
-                ), # need tuple here because torch behaves differently from numpy apparently
-                dims=(0,1)
-            )
+            # rolling over the boundaries
+            counts_int = self.torch_roll_boundaries(counts_int)
 
         return counts_int
 
-    def update_board(self):
-        # counting number of alive cells in neighbourhood (same shape)
-        count_ones_neighbours = self.fft_convolve2d()
+    '''
+    Main count_real operation in a single time-step
+    torch version using fft (less efficient)
+    '''
+    def torch_conv_fft(self):
+
+        # fft2 same shape but floating numbers
+        board_ft = torch_fft2(self.board)
+
+        # inverted fft2 (complex numbers)
+        inverted = torch_ifft2(board_ft * self.kernal_ft)
+
+        # get the real part of the complex argument (real number)
+        count_real = torch.real(inverted)
+
+        # round real part to closest integer
+        counts_int = torch.round(count_real).int()
+
+        if self.torus:
+            # rolling over the boundaries
+            counts_int = self.torch_roll_boundaries(counts_int)
+
+        return counts_int
+
+    '''
+    Rolling over the boundaries (torch version)
+    see https://en.wikipedia.org/wiki/Torus
+    '''
+    def torch_roll_boundaries(self, counts_int):
+        return torch.roll(
+            counts_int,
+            (
+                self.minus_shape_x_half_plus_one,
+                self.minus_shape_x_half_plus_one
+            ), # need tuple here because torch behaves differently from numpy apparently
+            dims=(0,1)
+        )
+
+    '''
+    Apply the rule (numpy version)
+    '''
+    def np_apply_rule(self, count_ones_neighbours):
+
         board_ones = self.board == 1
         board_zeros = ~ board_ones # negation
 
         new_board = np.zeros(self.shape)
+
         # rule[0] (survival): '1->1' based on count of "1" neighbours
         new_board[
             np.where(
@@ -173,6 +267,7 @@ class Automata:
                 board_ones # on cells
             )
         ] = 1
+
         # rule[1] (reproduction): '0->1' based on count of "1" neighbours
         new_board[
             np.where(
@@ -181,18 +276,20 @@ class Automata:
                 board_zeros # off cells
             )
         ] = 1
+
         # all other cells stay zeros (1->0, 0->0)
 
         self.board = new_board
 
-    def update_board_torch(self):
-
-        # counting number of alive cells in neighbourhood (same shape)
-        count_ones_neighbours = self.fft_convolve2d_torch()
+    '''
+    Apply the rule (torch version)
+    '''
+    def torch_apply_rule(self, count_ones_neighbours):
         board_ones = self.board == 1
         board_zeros = ~ board_ones # negation
 
-        new_board = torch.zeros(self.shape, dtype=torch.float32) # float
+        new_board = torch.zeros(self.shape, dtype=self.rule_dtype) # int or float
+
         # rule[0] (survival): '1->1' based on count of "1" neighbours
         new_board[
             torch.where(
@@ -201,6 +298,7 @@ class Automata:
                 board_ones # on cells
             )
         ] = 1
+
         # rule[1] (reproduction): '0->1' based on count of "1" neighbours
         new_board[
             torch.where(
@@ -209,22 +307,52 @@ class Automata:
                 board_zeros # off cells
             )
         ] = 1
+
         # all other cells stay zeros (1->0, 0->0)
 
         self.board = new_board
 
+    '''
+    Step update function using numpy
+    '''
+    def np_update_board(self):
+
+        # counting number of alive cells in neighbourhood (same shape)
+        # via fft or conv2 based on torch_conv_func
+        # (np_conv_fft or np_conv_conv2d)
+        count_ones_neighbours = self.numpy_conv_func()
+
+        # apply rule (update board inplace)
+        self.np_apply_rule(count_ones_neighbours)
+
+    '''
+    Step update function using torch
+    '''
+    def torch_update_board(self):
+
+        # counting number of alive cells in neighbourhood (same shape)
+        # via fft or conv2 based on torch_conv_func
+        # (torch_conv_fft or torch_conv_conv2d)
+        count_ones_neighbours = self.torch_conv_func()
+
+        # apply rule (update board inplace)
+        self.torch_apply_rule(count_ones_neighbours)
+
+    '''
+    Main Benchmark to run it as fast as possible (without visualizing it)
+    '''
     def benchmark(self, iterations):
-        start = time.process_time()
+        start = time.process_time() # TODO: check perf_counter() ?
 
         if self.use_torch:
             for _ in range(iterations):
-                self.update_board_torch()
-
+                self.torch_update_board()
         else:
+            # use numpy
             for _ in range(iterations):
-                self.update_board()
+                self.np_update_board()
 
-        ellapsed = time.process_time() - start
+        ellapsed = time.process_time() - start # TODO: check perf_counter() ?
 
         hz = iterations / ellapsed
 
@@ -233,32 +361,18 @@ class Automata:
         print(
             "Performed", iterations,
             "iterations of", self.shape,
-            f"cells in {ellapsed:.0f} sec:",
+            f"cells in {ellapsed} s",
             f"{hz:.0f} Hz (board)",
             f"{hz_B_cell:.2f} BHz (cell)"
         )
 
-    def save_last_frame(self, filename):
-        if self.use_torch:
-            self.board = self.board.cpu().detach().numpy()
-        np.save(filename, self.board)
-
-    def show_current_frame(self):
-        if self.use_torch:
-            self.board = self.board.cpu().detach().numpy()
-        self.image = pyplot.imshow(
-            self.board,
-            interpolation="nearest",
-            cmap=pyplot.cm.gray
-        )
-
-        pyplot.show()
-        # pyplot.savefig('lastframe.png')
-
+    '''
+    Show grid in real time (rely on numpy for now)
+    '''
     def animate(self, interval=100):
 
         def update_animation(*args):
-            self.update_board()
+            self.np_update_board()
             self.image.set_array(self.board)
             return self.image,
 
@@ -277,6 +391,23 @@ class Automata:
         )
         pyplot.show()
 
+    def save_last_frame(self, filename):
+        if self.use_torch:
+            self.board = self.board.cpu().detach().numpy()
+        np.save(filename, self.board)
+
+    def show_current_frame(self):
+        if self.use_torch:
+            self.board = self.board.cpu().detach().numpy()
+        self.image = pyplot.imshow(
+            self.board,
+            interpolation="nearest",
+            cmap=pyplot.cm.gray
+        )
+
+        pyplot.show()
+        # pyplot.savefig('lastframe.png')
+
 
 class Conway(Automata):
     def __init__(self, shape, board):
@@ -288,14 +419,14 @@ class Conway(Automata):
             [3]     # 'off->on': (3,): "on" neighbours (can't contain 0)
         ]
         # init automata
-        Automata.__init__(self, shape, board, neighborhood, rule)
+        Automata.__init__(self, board, neighborhood, rule)
 
 
 class Life34(Automata):
     def __init__(self, shape, board):
         neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
         rule = [[3, 4], [3, 4]]
-        Automata.__init__(self, shape, board, neighborhood, rule)
+        Automata.__init__(self, board, neighborhood, rule)
 
 
 class Amoeba(Automata):
@@ -309,7 +440,7 @@ class Anneal(Automata):
     def __init__(self, shape, board):
         neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
         rule = [[3, 5, 6, 7, 8], [4, 6, 7, 8]]
-        Automata.__init__(self, shape, board, neighborhood, rule)
+        Automata.__init__(self, board, neighborhood, rule)
 
 
 class Bugs(Automata):
@@ -319,7 +450,7 @@ class Bugs(Automata):
             np.arange(34, 59), # 'on->on': (2,3): "on" neighbours (can't contain 0)
             np.arange(34, 46)  # 'off->on': (3,): "on" neighbours (can't contain 0)
         ]
-        Automata.__init__(self, shape, board, neighborhood, rule)
+        Automata.__init__(self, board, neighborhood, rule)
 
 
 class Globe(Automata):
@@ -329,7 +460,7 @@ class Globe(Automata):
             np.arange(34, 59),
             np.arange(34, 46)
         ]
-        Automata.__init__(self, shape, board, neighborhood, rule)
+        Automata.__init__(self, board, neighborhood, rule)
 
 
 def test_torch_ca_moritztng():
@@ -401,7 +532,7 @@ def main_other_automata(
     board = rng.uniform(0, 1, shape)
     board = board < density
 
-    automata = automata_class(shape, board)
+    automata = automata_class(board)
 
     # interval=200 # ms
     interval = 0 # as fast as possible
@@ -419,6 +550,7 @@ def main_gol(
         animate = False, # if False do benchmark
         show_last_frame = False, # only applicable for benchmark
         save_last_frame = None, # only applicable for benchmark
+        use_fft = False,
         torch_device = None,
     ):
 
@@ -470,12 +602,12 @@ def main_gol(
 
     # init automata
     automata = Automata(
-        shape = shape,
         board = board,
         neighborhood = neighborhood,
         rule = rule,
         torus = torus,
-        torch_device = torch_device
+        use_fft = use_fft,
+        torch_device = torch_device,
     )
 
 
@@ -499,15 +631,16 @@ if __name__ == "__main__":
     # CONWAY GAME OF LIFE
     #
     main_gol(
-        shape_x = 1024,
+        shape_x = 2**10, #1024,
         initial_state = 'random', # 'square', 'filenmae.npy'
         density = 0.5, # only used with initial_state=='random'
         seed = 123, # only used with initial_state=='random'
-        iterations=10000,
+        iterations=1000,
         torus = True,
         animate = False,
         show_last_frame = False, # only applicable for benchmark
         save_last_frame = False, # '100k.npy'
+        use_fft = False, # or conv2d
         # torch_device = 'cpu', # torch cpu
         torch_device = 'cuda', # torch cuda
         # torch_device = 'mps', # torch mps
@@ -519,10 +652,11 @@ if __name__ == "__main__":
     ##############
     # BENCHMARKS
     #
-    # Benchmark 1000 iters, torus=True
-    # Numpy:                        23 Hz
+    # Benchmark 1000 iters, torus=True, conv2d (use_fft = False)
+    # Numpy:                        26 Hz
     # Torch mps:                   209 Hz
-    # Torch cuda (RTX 3090 Ti):   3295 Hz
+    # Torch cuda (RTX 3090 Ti):   1196 Hz (fft)
+    # Torch cuda (RTX 3090 Ti):   3294 Hz
     #
     ##############
 
