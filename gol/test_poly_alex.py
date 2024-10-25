8b839b0784cb97ba2a93271e0b7b13c6959fba5e
Federico <federico.sangati@gmail.com>
2024-10-25 23:11:40 +0900

    fixed poly (shift and norm)

--- gol/test_poly_alex.py
+++ gol/test_poly_alex.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import scipy
+from tqdm import tqdm
 from gol.pure.automata import Automata
 from gol.main_pure import init_gol_board_neighborhood_rule
 
@@ -11,7 +12,15 @@ def poly_solve(x,y):
     `y` is the number of alive neighbors.
     We only care about the sign information of the polynomial (to keep things simple, otherwise the polynomial would be much more complex). Numpy sign returns -1 and 1, so I shift it (add 1) and normalize (divide by 2) to get it back to 0 and 1
     '''
-    return -1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5)
+    return int(
+        (
+            np.sign(-1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5))
+            +1
+        )
+        /2
+    )
+
+    return
 
 def poly_advance(board, neighborhood):
     counts_int = scipy.signal.convolve2d(
@@ -21,25 +30,22 @@ def poly_advance(board, neighborhood):
             boundary = 'fill' # 'circular' if torus, 'fill' if strict
         )
     size = board.shape[0]
-    counts_int = np.rint(counts_int)
-    print('poly counts int')
-    print(counts_int)
-    new_board = np.zeros_like(counts_int)
+    counts_int = np.rint(counts_int).astype(int)
+    new_board = np.zeros_like(counts_int, dtype=int)
     for i in range(size):
         for j in range(size):
             x = board[i,j]
             y = counts_int[i,j]
             new_board[i,j] = poly_solve(x,y)
-    print('poly board 1')
-    print(new_board)
+    return new_board, counts_int
 
-def test():
+def test(size, seed):
 
     board, neighborhood, rule = init_gol_board_neighborhood_rule(
-        size = 4,
+        size = size,
         initial_state = 'random',
         density = 0.5,
-        seed = 123
+        seed = seed
     )
 
     automata = Automata(
@@ -51,17 +57,30 @@ def test():
         torch_device = None,
     )
 
-    automata.show_current_frame('test 0', force_show=False)
+    # automata.show_current_frame('test 0', force_show=False)
     board_0 = automata.get_board_numpy(change_to_int=True)
     automata.advance(1)
-    automata.show_current_frame('test 1', force_show=False)
+    # automata.show_current_frame('test 1', force_show=False)
     board_1 = automata.get_board_numpy(change_to_int=True)
-    print('board 0')
-    print(board_0)
-    print('board 1')
-    print(board_1)
-    # plt.show()
-    poly_advance(board_0, neighborhood)
+    poly_board_1, counts_int = poly_advance(board_0, neighborhood)
+    test = np.all(board_1==poly_board_1)
+    if not test:
+        # plt.show()
+        print('Test failed!')
+        print('board 0')
+        print(board_0)
+        print('board 1')
+        print(board_1)
+        print('poly counts int')
+        print(counts_int)
+        print('poly board 1')
+        print(poly_board_1)
+    return test
+
+
 
 if __name__ == "__main__":
-    test()
+    size = 4
+    for i in tqdm(range(1000)):
+        if not test(size=3, seed=1):
+            break
