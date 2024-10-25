9b01f07dd304260afe4f342c51745c6b3f8544a9
Federico <federico.sangati@gmail.com>
2024-10-25 23:27:06 +0900

    matrix operation (poly solve)

--- gol/test_poly_alex.py
+++ gol/test_poly_alex.py
@@ -5,23 +5,18 @@ from tqdm import tqdm
 from gol.pure.automata import Automata
 from gol.main_pure import init_gol_board_neighborhood_rule
 
-def poly_solve(x,y):
+def life_update(x,y):
     '''
     Credits to Raymond Baranski (Alex)
     `x` is the cell-state (dead=0, alive=1), and
     `y` is the number of alive neighbors.
     We only care about the sign information of the polynomial (to keep things simple, otherwise the polynomial would be much more complex). Numpy sign returns -1 and 1, so I shift it (add 1) and normalize (divide by 2) to get it back to 0 and 1
     '''
-    return int(
-        (
-            # np.sign(-1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5))
-            np.sign(-y**2 - x*y + 3.5*x + 6*y - 8.75)
-            +1
-        )
-        /2
-    )
-
-    return
+    return (
+        # np.sign(-1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5))
+        np.sign(-y**2 - x*y + 3.5*x + 6*y - 8.75)
+        +1
+    ) /2
 
 def poly_advance(board, neighborhood):
     size = board.shape[0]
@@ -32,12 +27,7 @@ def poly_advance(board, neighborhood):
             boundary = 'fill' # 'circular' if torus, 'fill' if strict
         )
     counts_int = np.rint(counts_int).astype(int)
-    new_board = np.zeros_like(counts_int, dtype=int)
-    for i in range(size):
-        for j in range(size):
-            x = board[i,j]
-            y = counts_int[i,j]
-            new_board[i,j] = poly_solve(x,y)
+    new_board = life_update(board, counts_int)
     return new_board, counts_int
 
 def test(size, seed):
