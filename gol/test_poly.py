759d919dea14d64ae35e97c85f55a9aa3b9e71b7
Federico <federico.sangati@gmail.com>
2024-10-27 16:44:00 +0900

    elaborated on poly (test) - using neighbours a,b,c,d,x,f,g,h,i

--- gol/test_poly.py
+++ gol/test_poly.py
@@ -12,9 +12,13 @@ def life_update(x,y):
     `x` is the cell-state (dead=0, alive=1), and
     `y` is the number of alive neighbors.
     We only care about the sign information of the polynomial (to keep things simple, otherwise the polynomial would be much more complex). Numpy sign returns -1 and 1, so I shift it (add 1) and normalize (divide by 2) to get it back to 0 and 1
+    f(x,y) = -1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5)
+    f(a,b,c,d,x,f,g,h,i) =
+       -(a+b+c+d+f+g+h+i)**2 - x*(a+b+c+d+f+g+h+i) + 3.5*x + 6*(a+b+c+d+f+g+h+i) - 8.75
+       =
+       -a**2 - f**2 - 2*a*b - 2*a*c - 2*a*d - 2*a*f - 2*a*g - 2*a*h - 2*i*a - b**2 - 2*b*c - 2*b*d - 2*b*f - 2*b*g - 2*b*h - 2*i*b - c**2 - 2*c*d - 2*c*f - 2*c*g - 2*c*h - 2*i*c - d**2 - i**2 - 2*d*f - 2*d*g - 2*d*h - 2*i*d - 2*f*g - 2*f*h - 2*i*f - g**2 - 2*g*h - 2*i*g - h**2 - 2*i*h - a*x - b*x - c*x - d*x - f*x - g*x - h*x - i*x + 3.5*x + 6*a + 6*b + 6*c + 6*d + 6*f + 6*g + 6*h + 6*i - 8.75
     '''
     return (
-        # np.sign(-1*(1-x)*(y-2.5)*(y-3.5)-x*(y-1.5)*(y-3.5))
         np.sign(-y**2 - x*y + 3.5*x + 6*y - 8.75)
         +1
     ) /2
